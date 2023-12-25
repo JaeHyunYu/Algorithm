3501eeeb858e62ff69bfc0aff2d6ebb570e767b1
YJH <slade12307@gmail.com>
2023-12-25 13:58:16 +0900

    finish level1

--- Baekjoon/Level1/Level1-1.py
+++ Baekjoon/Level1/Level1-1.py
@@ -1,9 +1,77 @@
 #print("Hello World!")
 
 '''
+2 사칙연산
+
 a,b=input().split()
 
 a=int(a)
 b=int(b)
 print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
+
+
 '''
+'''
+그외의 문제들
+a=input()
+print(a+"??!")
+
+----
+a= int(input())
+print(a-543)
+----
+a,b,c=map(int,input().split())
+
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+----
+
+a=int(input())
+b=int(input())
+b1=int(b/100)
+b2=int((b-b1*100)/10)
+b3=b-b1*100-b2*10
+
+
+print(b3*a)
+print(b2*a)
+print(b1*a)
+print(a*b)
+
+----
+
+a,b,c=map(int,input().split())
+print(a+b+c)
+
+----
+print("\\    /\\")
+print(" )  ( \') ")
+print("(  /  ) ")
+print(" \\(__)| ")
+----
+
+
+'''
+
+
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
+
+
+
+
+
+
+
+
+
