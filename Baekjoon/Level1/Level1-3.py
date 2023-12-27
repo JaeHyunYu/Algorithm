090714cd3fc7a1d14becb14fb6d9c899d09896cd
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2023-12-27 14:50:34 +0900

    Update Level1-3.py
    finish 반복문(EOF중요)

--- Baekjoon/Level1/Level1-3.py
+++ Baekjoon/Level1/Level1-3.py
@@ -44,4 +44,39 @@ n=int(sys.stdin.readline())
 for i in range(n):
     a,b=map(int,sys.stdin.readline().split())
     print(a+b)
+    
+---
+a=int(input())
+for i in range(1,(a+1)):
+	b,c=map(int,input().split())
+	print("Case #",i,": ",b+c,sep="")
+---
+a=int(input())
+for i in range(1,(a+1)):
+	b,c=map(int,input().split())
+	print("Case #",i,": ",b," + ",c," = ",b+c,sep="")
+---
+a=int(input())
+for i in range(1,(a+1)):
+    print("*"*i)
+---
+a=int(input())
+for i in range(1,(a+1)):
+    print(" "*(a-i),"*"*i,sep="")
+---
+while(1):
+    a,b=map(int,input().split())
+    if(a==b==0):
+        break
+    print(a+b)
+
+---
+while(1):
+    try:
+        a,b=map(int,input().split())
+        print(a+b)
+    except:
+        break
+
+#EOF문제
 '''
