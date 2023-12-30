f8ac2785934b52a084487738a6fb4e0df4d80d26
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2023-12-30 16:24:43 +0900

    Update Level1-7(2차원 배열).py
    solve 3. 세로읽기

--- Baekjoon/Level1/Level1-7(2차원 배열).py
+++ Baekjoon/Level1/Level1-7(2차원 배열).py
@@ -28,5 +28,53 @@ for i in range(a):
     print()
 
 ---
+2. 최댓값(2566)
+최댓값을 2차원에서 찾는 문제
+
+arr1=[[0 for j in range(9)] for i in range(9)]
+
+a=b=0
+max_v=0
+
+
+for i in range(9):
+    n=map(int,input().split())
+    n=list(n)
+    for j in range(9):
+        if(max_v<=int(n[j])):
+            max_v=int(n[j])
+            a,b=(i+1),(j+1)
+        arr1[i][j]=n[j]     
+
+print(max_v)
+print(a,b)
+
+---
+3. 세로읽기(10798)
+문자열의 배열을 다루는 문제
+
+arr=[]
+mn=0
+
+for i in range(5):
+    arr.append(list(input()))
+    if(len(arr[i])>=mn):
+        mn=len(arr[i])
+               
+
+
+for i in range(mn):
+    for j in range(5):
+        if(len(arr[j])<=int(i)):
+            continue
+        else:
+            print(arr[j][i],end=""
+
+---
+4. 색종이(2563)
+2차원 배열을 활용하여 색종이로 평면을 덮는 문제
+
+
+
 '''
 
